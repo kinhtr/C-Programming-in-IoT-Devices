3c7db24afb6e49918319ffc5a5c3852f70560fbb
kinhtr <88277180+kinhtr@users.noreply.github.com>
2021-12-17 01:27:58 +0200

    Update and rename Exercises/Use va_arg to count the number of string variable arguments to Exercises/Use va_arg to count the number of string arguments/count_args.c

--- Exercises/Use va_arg to count the number of string arguments/count_args.c
+++ Exercises/Use va_arg to count the number of string arguments/count_args.c
@@ -1,3 +1,8 @@
+/*The variabale arguments are pointers to strings. 
+The end of arguments is indicated by a NULL pointer. 
+The function returns the number of strings. 
+Function also counts total number of characters in the strings and stores the total through the first parameter.*/
+
 int count_args(int *number, ...) {
     int count = 0;
     int i;
