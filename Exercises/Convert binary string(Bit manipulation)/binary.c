8c9a432f277d73d7ed4b79f93d8c12486c2ea37f
kinhtr <88277180+kinhtr@users.noreply.github.com>
2021-12-17 01:18:58 +0200

    Update binary.c

--- Exercises/Convert binary string(Bit manipulation)/binary.c
+++ Exercises/Convert binary string(Bit manipulation)/binary.c
@@ -30,17 +30,3 @@ bool binary_reader(unsigned int *pu) {
     }
     return false;
 }
-
-//Digit counter returns the minimum number of hexadecimal digits that are needed to print the number given as parameter
-
-int digit_counter(unsigned int nr) {
-    int count = 0;
-    while (nr > 0) {
-        nr = nr >> 4;
-        count++;
-    }
-    if (count == 0) {
-        count = 1;
-    }
-    return count;
-}
